6f5b997011356c2c2e46a657486b36e5f3453b0d
roobon3d <roobon3d@gmail.com>
2019-06-12 18:51:14 +0200

    hacer fichajes de prueba!!!!!!!!!!!!!!!!!!!1

--- app/src/main/java/edu/cftic/fichapp/FichajeActivity.java
+++ app/src/main/java/edu/cftic/fichapp/FichajeActivity.java
@@ -6,14 +6,21 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 
+import edu.cftic.fichapp.bean.Empleado;
+import edu.cftic.fichapp.bean.Empresa;
 import edu.cftic.fichapp.bean.Fichaje;
+import edu.cftic.fichapp.persistencia.DB;
+import edu.cftic.fichapp.util.Constantes;
 
 public class FichajeActivity extends AppCompatActivity {
 
-    ArrayList<Fichaje> listaFichajes;
-    RecyclerView recyclerFichajes;
+    private ArrayList<Fichaje> listaFichajes;
+    private RecyclerView recyclerFichajes;
+    private RecyclerView.LayoutManager layoutManager;
 
 
     @Override
@@ -21,9 +28,9 @@ public class FichajeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fichaje);
 
-
+        fichajesPruebas();
         construirRecycler();
-        Log.d ("FichApp","construir recycler");
+        Log.d("FichApp", "construir recycler");
     }
 
     private void construirRecycler() {
@@ -34,12 +41,44 @@ public class FichajeActivity extends AppCompatActivity {
 
         recyclerFichajes.setLayoutManager(new LinearLayoutManager(this));
 
-        AdapterFichaje adapter = new AdapterFichaje(listaFichajes);
+        AdapterFichaje adapter = new AdapterFichaje(this, listaFichajes);
 
         recyclerFichajes.setAdapter(adapter);
 
     }
 
+    private void fichajesPruebas()
+    {
+
+        //TODO Crear fichajes para pruebas
+
+
+        Empresa em = new Empresa("B123456", "XYZYZ SA", "T T", "xyz@xyz.com");
+        //boolean v = DB.empresas.nuevo(em);
+        Empleado nu = DB.empleados.getEmpleadoUsuarioClave("", "");
+        ArrayList<Empresa> ae = (ArrayList<Empresa>) DB.empresas.getEmpresas();
+        em = DB.empresas.ultimo();
+        Empleado tr = new Empleado("JUAN YONG 2", "JYON3", "12345", "B", false, em);
+        boolean t = DB.empleados.nuevo(tr);
+        ArrayList<Empleado> at = (ArrayList<Empleado>) DB.empleados.getEmpleados();
+
+        tr = DB.empleados.ultimo();
+        Log.i(Constantes.TAG_APP, "E: " + tr);
+        for (Empleado es : at) {
+            Log.i(Constantes.TAG_APP, "= " + es);
+        }
+        at = (ArrayList<Empleado>) DB.empleados.getEmpleados();
+
+        Timestamp de = new Timestamp(new Date().getTime());
+        Timestamp hasta = new Timestamp(new Date().getTime());
+
+        Fichaje fe = new Fichaje(tr, de, hasta, "Mensaje");
+        Log.i(Constantes.TAG_APP, "F: " + fe);
+        boolean d = DB.fichar.nuevo(fe);
+        ArrayList<Fichaje> af = (ArrayList<Fichaje>) DB.fichar.getFicheje(tr.getId_empleado());
+
+
+    }
 
 }
 
